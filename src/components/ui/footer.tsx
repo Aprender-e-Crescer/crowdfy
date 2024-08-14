b20d22ad10c69eb9fb296843c68a036a2af06bfe
Samuel Molinari <samuelmolinari8@gmail.com>
2024-08-14 20:50:08 -0300

    iniciando o footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,21 @@
-export default function Footer(){
-    return (
-        <div>
-            oi samuel
-        </div>
-    )
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+
+export default function Footer() {
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-gray h-44 flex items-center justify-center">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  )
 }
