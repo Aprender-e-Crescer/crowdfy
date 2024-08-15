78c333e79a3e9b88600db76885bc67c4f7cc9ff8
Samuel Molinari <samuelmolinari8@gmail.com>
2024-08-14 21:28:20 -0300

    arrumando o footer, falta colocar as opcoes do figma e fazer com que quando abra a div aumente tambem.

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -7,15 +7,72 @@ import {
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-gray h-44 flex items-center justify-center">
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <div className="flex bg-black flex-wrap justify-around h-auto ">
+      <div className=" bg-transparent text-white h-20 flex items-center justify-center p-4">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Home</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className=" bg-transparent text-white h-20 flex items-center justify-center p-4">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Explore</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className=" bg-transparent text-white h-20 flex items-center justify-center p-4">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Campaigns</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className=" bg-transparent text-white h-20 flex items-center justify-center p-4">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-4">
+            <AccordionTrigger>Profile</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+      <div className=" bg-transparent text-white h-20 flex items-center justify-center p-4">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-5">
+            <AccordionTrigger>Resources</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   )
 }
