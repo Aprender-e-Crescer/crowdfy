ec51726f8e5717954777016d711bece2e96f4f30
Raul Machado <rauldondonimachado09@gmail.com>
2024-08-15 19:01:23 -0300

    Manutenção-cardDonate

--- src/routes/my-campaigns.tsx
+++ src/routes/my-campaigns.tsx
@@ -7,8 +7,10 @@ export const Route = createFileRoute('/my-campaigns') ({
 export function LiveCampaigns() {
     return (
     <>
-    <div className='m-5 shadow-md shadow-gray-400 rounded '>
-        <h1>🫰Funds donated to this campaign</h1>
+    <div className=' m-5 p-3 shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
+        <h1 className='font-semibold'>🫰Funds donated to this campaign</h1>
+        <p className='text-3xl'>$15.000 /<span className='font-bold'> $20.000</span></p>
+        <div className="my-3 w-[360px] h-2 bg-[#84CC16] rounded-full" />    
     </div>
     </>
     )
