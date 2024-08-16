0bfa0b874e6ff10d414fd16720d600b0811e2dc8
Raul Machado <rauldondonimachado09@gmail.com>
2024-08-16 17:02:15 -0300

    Button-Share

--- src/routes/my-campaigns.tsx
+++ src/routes/my-campaigns.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Input } from "@/components/ui/input"
 import { SubHeader } from '@/components/subHeader'
+import { Button } from '@/components/ui/button'
+import { DESTRUCTION } from 'dns'
+import share from '@/assets/share.svg'
 
 /*Arrumar o label  e  estilizar o input*/
 export const Route = createFileRoute('/my-campaigns') ({
@@ -13,13 +16,15 @@ export function LiveCampaigns() {
     <SubHeader />
 
 
-    <div className=' m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
+    <div className='font-[inter] m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
         <h1 className='text-[#475467] mt-1 mb-4 font-semibold text-xl'>🫰Funds donated to this campaign</h1>
         <p className='text-3xl'>$15.000 /<span className='font-bold'> $20.000</span></p>
         <div className="my-3 w-max-[360px] h-2 bg-[#84CC16] rounded-full"></div> 
-        <p className='text-base'>185 doações</p>   
+        <p className='text-base'>185 doações</p>
+        <Button className='max-w-40 border-black ' variant={'shadow'}><img src={share} className='mx-2' />Share Campaign</Button>
     </div>
-    <div className=' m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
+        {/*Começo do segundo card de financie esta campanha */}
+    <div className='font-[inter] m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
     <h1 className='mt-1 mb-4 font-semibold text-xl text-[#475467]'>💪 Fund this campaign</h1>
     <label htmlFor="Fund">
     <h1 itemID='Fund' className='text-[#0F172A] ml-1 font-medium'>Funding</h1>
