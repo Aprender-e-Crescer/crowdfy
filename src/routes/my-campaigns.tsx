c28cae3c37e9d0d98fbc361e07385bb2c841a2a4
Raul Machado <rauldondonimachado09@gmail.com>
2024-08-15 22:09:11 -0300

    CardFund-input

--- src/routes/my-campaigns.tsx
+++ src/routes/my-campaigns.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Input } from "@/components/ui/input"
+
 
 export const Route = createFileRoute('/my-campaigns') ({
     component: LiveCampaigns
@@ -7,10 +9,15 @@ export const Route = createFileRoute('/my-campaigns') ({
 export function LiveCampaigns() {
     return (
     <>
-    <div className=' m-5 p-3 shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
-        <h1 className='font-semibold'>🫰Funds donated to this campaign</h1>
+    <div className=' m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
+        <h1 className='mt-1 mb-4 font-semibold text-xl'>🫰Funds donated to this campaign</h1>
         <p className='text-3xl'>$15.000 /<span className='font-bold'> $20.000</span></p>
-        <div className="my-3 w-[360px] h-2 bg-[#84CC16] rounded-full" />    
+        <div className="my-3 w-max-[360px] h-2 bg-[#84CC16] rounded-full"></div> 
+        <p className='text-base'>185 doações</p>   
+    </div>
+    <div className=' m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
+    <h1 className='mt-1 mb-4 font-semibold text-xl'>💪 Fund this campaign</h1>
+    <Input/>
     </div>
     </>
     )
