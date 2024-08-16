b2a3d5d543b2ec8114c067e1fc38fc17883af619
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-08-15 22:24:27 -0300

    corrigindo o mapa

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,7 +1,9 @@
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+
 export function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className='w-full h-full' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
