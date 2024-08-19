9614c659d7ee3c72d53e77f28df598ff0090f8e2
Ariel <arielcristiandemenezes4@gmail.com>
2024-08-19 16:34:13 -0300

    terminado-sem-responsividade

--- src/components/headerCard.tsx
+++ src/components/headerCard.tsx
@@ -3,25 +3,28 @@ import Piggy_Bank from "../assets/piggy-bank.png";
 import arrowUp from "../assets/arrow-up.png";
 import arrowDown from "../assets/arrow-down.png";
 import filter from "../assets/filter2.png";
+import list from "../assets/list.png";
+import markerPin from "../assets/marker-pin-02.png";
+
 
 export function HeaderCard() {
   return (
     <div className="p-4 ">
-      <div className="flex items-center mb-4">
+      <div className="flex items-center mb-4 ">
         <div className="flex flex-col mr-8 ">
           <h1 className="font-bold text-3xl pb-2">Explore</h1>
           <p className="text-[#64748B]">Where do you want to help</p>
         </div>
-        <div className="flex space-x-0"> 
-          <button className="border-2 border-[#D1D5DB] rounded-l-lg px-4 py-2 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0]">
+        <div className="flex pl-28"> 
+          <button className="border-2 border-[#D1D5DB] rounded-l-lg px-4 py-2 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0] border-r-0">
             <img src={arrowUp} alt="Price Icon" className="w-6 h-6 mr-2" />
             Price
           </button>
-          <button className="border-2 border-[#D1D5DB] px-4 py-2 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0]">
+          <button className="border-2 border-[#D1D5DB] px-4 py-2 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0] rounded-r-lg">
             <img src={arrowDown} alt="Date Icon" className="w-6 h-6 mr-2" />
             Date
           </button>
-          <button className="bg-[#84CC16] rounded-r-lg px-4 py-2 text-[#ffffff] font-medium flex items-center hover:bg-[#098716]">
+          <button className="bg-[#84CC16] rounded-lg px-4 py-2 text-[#ffffff] font-medium flex items-center hover:bg-[#098716] ml-4">
             <img src={filter} alt="Show Filters Icon" className="w-6 h-6 mr-2" />
             Show filters
           </button>
@@ -29,10 +32,10 @@ export function HeaderCard() {
       </div>
       <div>
         <div className="flex space-x-0">
-          <button className="border-2 border-[#D1D5DB] rounded-l-lg px-4 py-2 bg-[#ffffff] font-medium flex items-center text-[#334155] hover:bg-[#E2E8F0]">
+          <button className="border-2 border-[#D1D5DB] rounded-l-lg px-4 py-2 bg-[#ffffff] font-medium flex items-center text-[#334155] hover:bg-[#E2E8F0] border-r-0">
             All views
           </button>
-          <button className="border-2 border-[#D1D5DB] px-4 py-2 bg-[#F1F5F9] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0]">
+          <button className="border-2 border-[#D1D5DB] px-4 py-2 bg-[#F1F5F9] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0] border-r-0">
             <img src={fileSignature} alt="Petitions Icon" className="w-6 h-6 mr-2" />
             Petitions
           </button>
@@ -40,6 +43,14 @@ export function HeaderCard() {
             <img src={Piggy_Bank} alt="Donations Icon" className="w-6 h-6 mr-2" />
             Donations
           </button>
+          <div className="flex pl-[236px]"> 
+          <button className="border-2 border-[#D1D5DB] rounded-l-lg px-3 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0] border-r-0">
+            <img src={list} alt="Price Icon" className="w-6 h-6" />
+          </button>
+          <button className="border-2 border-[#D1D5DB] px-3 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0] rounded-r-lg">
+            <img src={markerPin} alt="Date Icon" className="w-6 h-6" />
+          </button>
+          </div>
         </div>
       </div>
     </div>
