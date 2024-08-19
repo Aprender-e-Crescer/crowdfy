0be99e7c568a87c91917505dff2fa1824c9d4e83
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-08-19 19:38:53 -0300

    correção de bugs e remoção e arquivos desnecessários

--- src/routes/my-campaigns.tsx
+++ src/routes/my-campaigns.tsx
@@ -130,19 +130,7 @@ export function LiveCampaigns() {
                 <p className="font-medium">Denunciar esta campanha</p> 
             </div>
     
-            <div className='m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
-                  <h1 className='text-[#475467] mt-1 mb-4 font-semibold text-xl'>🫰Funds donated to this campaign</h1>
-                  <p className='text-3xl'>$15.000 /<span className='font-bold'> $20.000</span></p>
-                  <div className="my-3 w-max-[360px] h-2 bg-[#84CC16] rounded-full"></div> 
-                  <p className='text-base'>185 doações</p>   
-              </div>
-              <div className=' m-5 py-2 px-3 border shadow-md shadow-gray-400 rounded-[12px] max-w-96'>
-              <h1 className='mt-1 mb-4 font-semibold text-xl text-[#475467]'>💪 Fund this campaign</h1>
-              <label htmlFor="Fund">
-                  <h1 itemID='Fund' className='text-[#0F172A] ml-1 font-medium'>Funding</h1>
-                  <Input/> 
-              </label>
-            </div>
+            
     </>
     )
 }
