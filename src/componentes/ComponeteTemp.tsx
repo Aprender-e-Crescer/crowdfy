9cfb6e0069dd676743177ed94db86248d552a41d
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-08-19 20:45:07 -0300

    Desenvolvido componente ViewActivity

---
+++ src/componentes/ComponeteTemp.tsx
@@ -0,0 +1,4 @@
+<div className="flex justify-between items-center mb-1">
+        <h1 className="text-sm font-bold text-gray-900">Activity</h1>
+        <h2 className="text-xs font-medium text-gray-500">View all</h2>
+      </div>
