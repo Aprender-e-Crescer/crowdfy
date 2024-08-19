a7faf7ab637317f11fdfccff85960f8711576d12
Natanael12-43 <natanaelmaciel395@gmail.com>
2024-08-19 20:59:55 -0300

    iniciandofavorites

--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Sectionheader } from '@/components/component/sectionheader'
 
 export const Route = createFileRoute('/explore')({
   component: Explore,
 })
 
 function Explore() {
-  return <div className="p-2">Hello from About!</div>
+  return (
+    <>
+      <Sectionheader />
+    </>
+  )
 }
