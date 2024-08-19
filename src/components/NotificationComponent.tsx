d815efd56045dc9d5fd8ff5540cd57719188922e
belloli0 <felipebelloli4@gmail.com>
2024-08-19 20:49:35 -0300

    Notification-Component

---
+++ src/components/NotificationComponent.tsx
@@ -0,0 +1,45 @@
+
+import connector from "@/assets/connector.png"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import img from "@/assets/img.png"
+
+
+export function NotificationComponent({ nome, postedIn, text1, text2 }: { nome: string, postedIn: string, text1: string, text2: string }) {
+    return (
+        <div className="flex gap-6  max-[415px]:flex">
+            <div className="flex flex-col pt-6 justify-center">
+                <div>
+                    <Avatar className="flex justify-center items-center size-14  ">
+                        <AvatarImage className="bg-lime-100 " src={img} />
+                        <AvatarFallback></AvatarFallback>
+                    </Avatar>
+                    <div className="flex items-center justify-center">
+                        <img src={connector} alt="aaaa" />
+                    </div>
+                </div>
+                <div className="line-clamp-2">
+                </div>
+            </div>
+            <div className="flex flex-col  gap-3 pt-6 ">
+                <div className="flex gap-3 items-center ">
+                    <p className="text-[#475467] text-[20px] font-medium">{nome}</p>
+                    <p className="text-[#94A3B8]">{postedIn}</p>
+                </div>
+                <div className="flex gap-3 ">
+                    <p className="flex gap-3 text-[#475569]">{text1}</p>
+                </div>
+                <div className="flex gap-3 ">
+                    <p className="text-[#475569]">{text2}</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    )
+}
+
+
+
+
--- src/routes/profileNotifications.tsx
+++ src/routes/profileNotifications.tsx
@@ -1,3 +1,5 @@
+import img from '@/assets/img.png'
+import { NotificationComponent } from '@/components/NotificationComponent'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(`/profileNotifications`)({
@@ -6,6 +8,12 @@ export const Route = createFileRoute(`/profileNotifications`)({
 
 function ProfileNotifications() {
   return <div className="p-2">
-    Coloquem seus Componentes aqui para testar
+
+    <NotificationComponent
+      nome='Matheus Rodrigues'
+      text1='is gathering 9000 signs to citys prefecture'
+      postedIn='in 20 days' 
+      text2='“We need to deliver this about of 9.000 signs to the mayor to he approve us as
+       international english teacher”'/>
   </div>
 }
