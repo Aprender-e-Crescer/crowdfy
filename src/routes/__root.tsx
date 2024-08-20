529779ffafedc6754f8d69a89976c2764849de24
JoaoPMBsp <joaopedromoraesbonkoski@gmail.com>
2024-08-20 20:45:35 -0300

    Adicionado o componente Titulo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import ViewActivity from '@/componentes/Activity'
+import Titulo from '@/componentes/Titulo'
 
 export const Route = createRootRoute({
   component: () => (
     <>
      
-   
+      <Outlet />
+      <Titulo />
       <ViewActivity />
       <ViewActivity />
       <ViewActivity />
@@ -16,6 +18,8 @@ export const Route = createRootRoute({
       <ViewActivity />
       <ViewActivity />
       <ViewActivity />
+      <TanStackRouterDevtools />
+      <Link />
     </>
     
   ),
