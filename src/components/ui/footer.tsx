3cf6b6d0d74828bdfc11eb37320759c3b9acccaa
enzoguis <enzog0315@gmail.com>
2024-08-19 21:48:36 -0300

    indentacao

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -14,54 +14,61 @@ export default function Footer() {
       <div className="hidden md:flex bg-slate-100 w-full flex-col">
         <div className="flex justify-center gap-x-16 py-20 flex-1 px-8">
           <div className="flex flex-col flex-1 items-start max-w-40">
-            <h1 className='text-lime-700 font-semibold'>Home</h1>
-
-            <p className='text-slate-600'>My favorites</p>
-            <p className='text-slate-600'>Recents</p>
+            <h1 className="text-lime-700 font-semibold">Home</h1>
+            <p className="text-slate-600">My favorites</p>
+            <p className="text-slate-600">Recents</p>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
-            <h1 className='font-semibold'>Explore</h1>
+            <h1 className="font-semibold">Explore</h1>
 
-            <p className='text-slate-600'>Lists</p>
-            <p className='flex text-slate-600'>Maps <Badge variant="success">New</Badge></p>
+            <p className="text-slate-600">Lists</p>
+            <p className="flex text-slate-600">
+              Maps <Badge variant="success">New</Badge>
+            </p>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
-            <h1 className='font-semibold'>Campaigns</h1>
+            <h1 className="font-semibold">Campaigns</h1>
 
-            <p className='text-slate-600 ' >Home</p>
-            <p className='flex text-slate-600'>Campaigns <Badge variant="success">12</Badge></p>
-            <p className='flex text-slate-600'>Donations <Badge variant="success">4</Badge></p>
-            <p className='text-slate-600'>Analytics</p>
+            <p className="text-slate-600 ">Home</p>
+            <p className="flex text-slate-600">
+              Campaigns <Badge variant="success">12</Badge>
+            </p>
+            <p className="flex text-slate-600">
+              Donations <Badge variant="success">4</Badge>
+            </p>
+            <p className="text-slate-600">Analytics</p>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
-            <h1 className='font-semibold'>Profile</h1>
+            <h1 className="font-semibold">Profile</h1>
 
-            <p className='text-slate-600'>Settings</p>
-            <p className='flex text-slate-600'>Notifications <Badge variant="success">10</Badge></p>
+            <p className="text-slate-600">Settings</p>
+            <p className="flex text-slate-600">
+              Notifications <Badge variant="success">10</Badge>
+            </p>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
-            <h1 className='font-semibold'>Resources</h1>
+            <h1 className="font-semibold">Resources</h1>
 
-            <p className='text-slate-600'>How to use crouwdfy</p>
-            <p className='text-slate-600'>Docs</p>
-            <p className='text-slate-600'>Legal Terms</p>
-            <p className='text-slate-600'>Blog</p>
-            <p className='text-slate-600'>Merch</p>
+            <p className="text-slate-600">How to use crouwdfy</p>
+            <p className="text-slate-600">Docs</p>
+            <p className="text-slate-600">Legal Terms</p>
+            <p className="text-slate-600">Blog</p>
+            <p className="text-slate-600">Merch</p>
           </div>
         </div>
         <hr />
         <div className="flex mx-20 py-5 items-center justify-between">
-            <div className="flex gap-x-2">
-              <img src={iconfooter} alt="" />
-              <p className="font-bold text-lg">Crowdfy</p>
-            </div>
-            <div className='flex'>
-              <p className='flex flex-wrap text-[#667085]'>© 2077 Mainnet Design. All rights reserved.</p>
-            </div>
+          <div className="flex gap-x-2">
+            <img src={iconfooter} alt="" />
+            <p className="font-bold text-lg">Crowdfy</p>
           </div>
-        
+          <div className="flex">
+            <p className="flex flex-wrap text-[#667085]">
+              © 2077 Mainnet Design. All rights reserved.
+            </p>
+          </div>
+        </div>
       </div>
-      
 
       <div className=" md:hidden flex flex-wrap bg-slate-100">
         <Accordion type="single" collapsible className="w-full">
@@ -141,8 +148,11 @@ export default function Footer() {
               <img src={iconfooter} alt="" />
               <p className="font-bold text-lg">Crowdfy</p>
             </div>
-            <div className='flex'>
-              <p className='flex flex-wrap text-[#667085] font-light'>© 2077 Mainnet Design.<br /> All rights reserved.</p>
+            <div className="flex">
+              <p className="flex flex-wrap text-[#667085] font-light">
+                © 2077 Mainnet Design.
+                <br /> All rights reserved.
+              </p>
             </div>
           </div>
         </Accordion>
