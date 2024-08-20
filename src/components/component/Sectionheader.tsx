7a66cee43af474f2e537845468363d1a81fee532
Natanael12-43 <natanaelmaciel395@gmail.com>
2024-08-19 21:00:31 -0300

    iniciando favoritos

--- src/components/component/Sectionheader.tsx
+++ src/components/component/Sectionheader.tsx
@@ -6,7 +6,7 @@ import { IoGridOutline } from "react-icons/io5";
 
 export function Sectionheader() {
   return (
-    <div className="space-y-2 py-6 flex">
+    <div className="space-y-2 py-6">
       <h2 className="text-3xl font-bold tracking-tight">Favorites</h2>
       <p className="text-muted-foreground">Como você pretende ajudar o mundo hoje.</p>
       <button className="border-2 border-[#D1D5DB] rounded-lg px-4 py-2 bg-[#ffffff] text-[#334155] font-medium flex items-center hover:bg-[#E2E8F0]">
