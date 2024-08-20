09bd75d728952a4a31a33afe337558a1399fe60e
unknown <cantellidumbo@gmail.com>
2024-08-19 23:06:55 -0300

    CARROSSEL 3 FINALIZADO

--- src/components/ui/give.tsx
+++ src/components/ui/give.tsx
@@ -1,23 +1,23 @@
-export default function Give() {
+export default function Give({title, subtitle, label1, label2, label3, placeholder1, placeholder2, placeholder3} : {title: string, subtitle: string, label1: string, label2: string, label3: string, placeholder1: string, placeholder2: string, placeholder3: string}) {
     return (
         <div className="flex bg-white flex-col">
             <div className="text-black font-semibold text-2xl">
-                <h2>3. Give Final Details</h2>
+                <h2>{title}</h2>
                 <div className="text-xl font-normal text-[#475467]">
-                    <h2>What it's about your campaign?</h2>
+                    <h2>{subtitle}</h2>
                 </div>
                 <div className=" mb-6 mt-12">
                     <label 
                         htmlFor="campaign-title" 
                         className="block mb-4 text-xl font-medium text-[#1E293B] dark:text-white"
                     >
-                        Your campaign title
+                        {label1}
                     </label>
                     <input 
                         type="text" 
                         id="campaign-title"
                         className="flex flex-col font-light border border-gray-300 rounded-sm w-80 h-10 placeholder-gray-400 placeholder:text-lg text-xl " 
-                        placeholder="Write here your beautiful title 🪴" 
+                        placeholder={placeholder1} 
                         required
                     />
                 </div>
@@ -26,13 +26,13 @@ export default function Give() {
                         htmlFor="campaign-date" 
                         className="block mb-4 text-xl font-medium text-black dark:text-white"
                     >
-                        Campaign date
+                        {label2}
                     </label>
                     <input 
                         type="text" 
                         id="campaign-date"
                         className="flex flex-col font-light border border-gray-300 rounded-sm w-80 h-10 placeholder-gray-400 placeholder:text-xl text-xl" 
-                        placeholder="dd/mm/aaaa" 
+                        placeholder={placeholder2} 
                         required
                     />
                 </div>
@@ -41,13 +41,13 @@ export default function Give() {
                         htmlFor="campaign-goal" 
                         className="block mb-4 text-xl font-medium text-black dark:text-white"
                     >
-                        Goal
+                        {label3}
                     </label>
                     <input
                         type="text" 
                         id="campaign-goal"
                         className="flex flex-col font-light border border-gray-300 rounded-sm w-80 h-10 placeholder-gray-400 placeholder:text-xl text-xl" 
-                        placeholder="$10,000" 
+                        placeholder={placeholder3} 
                         required
 
                     />
--- src/routes/CampaingCreate.tsx
+++ src/routes/CampaingCreate.tsx
@@ -8,7 +8,14 @@ export const Route = createFileRoute('/CampaingCreate')({
 function CampaingCreate() {
   return (
   
-  <Give />
+  <Give title='3. Give final details'
+        subtitle="What it's about your campaign?"
+        label1='Your campaign title'
+        label2='Create a deadline for your campaign'
+        label3='Goal'
+        placeholder1='Write here your beautiful title 🪴'
+        placeholder2='dd/mm/aaaa'
+        placeholder3='$10.000'/>
         
 
 
