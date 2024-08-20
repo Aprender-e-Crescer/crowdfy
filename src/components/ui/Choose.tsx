ee623c67252ad45c02b1822530023f1e8da3a235
EduardoMakximovtz <makximovtzeduardo@gmail.com>
2024-08-19 21:52:08 -0300

    Corrigindo Bugs

--- src/components/ui/Choose.tsx
+++ src/components/ui/Choose.tsx
@@ -1,19 +1,19 @@
 import  { PiggyBank,FilePenLine }  from 'lucide-react';
 
-export default function Choose(){
+export default function Choose({tittle, subtittle, donation, petition, descrição1, descrição2}: {tittle: string, subtittle: string, donation: string, petition: string, descrição1: string, descrição2: string}){
   return(
     <div className="flex flex-col gap-4">
             <div className="flex flex-col">
                 <div className="flex">
-                    <p className="text-2xl text-[#101828] font-bold">1. Choose your campaign style</p>
+                    <p className="text-2xl text-[#101828] font-bold">{tittle}</p>
                 </div>
                 <div className="flex">
-                    <p className="text-[#475467] font-normal text-[16px]">What it's about your campaign?</p>
+                    <p className="text-[#475467] font-normal text-[16px]">{subtittle}</p>
                 </div>
             </div>
             <div className="flex gap-1">
-                <div className="flex w-[320px] h-[593px] border-b-2 border-t-2 border-l-2 border-r-2 rounded-xl items-center ">
-                    <div className="flex-col pt-96 pl-5">
+                <div className="flex w-[320px] h-[165px] border-b-2 border-t-2 border-l-2 border-r-2 rounded-xl items-center ">
+                    <div className="flex-col pt-2 pl-4 justify-center items-center">
                         <div className="">
                           <div className=""></div>
                           <div className="flex items-center justify-center border-4 border-slate-100 bg-[#FFFFFF] rounded-full w-16">
@@ -21,28 +21,27 @@ export default function Choose(){
                               </PiggyBank>
                               </div>
                             <div className="flex">
-                                <p className="text-[35px] font-bold">Donation</p>
+                                <p className="text-[24px] font-bold">{donation}</p>
                             </div>
                             <div className="flex">
-                                <p className="text-[#64748B]">Create a donation to receive <br />
-                                 funds</p>
+                                <p className="text-[#64748B] text-[16px]">{descrição1}
+                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex w-[320px] h-[593px] border-b-2 border-t-2 border-l-2 border-r-2 rounded-xl items-center ">
-                <div className="flex-col pt-96 pl-5">
+                <div className="flex w-[320px] h-[165px] border-b-2 border-t-2 border-l-2 border-r-2 rounded-xl items-center ">
+                <div className="flex-col pt-2 pl-4">
                         <div className="">
                         <div className="flex items-center justify-center border-4 border-slate-100 bg-[#FFFF] rounded-full w-16 ">
                           <FilePenLine size={56} className="text-slate-500 bg-gray-200 p-2 rounded-full ">
                           </FilePenLine>
                           </div>
                             <div className="flex">
-                                <p className="text-[35px] font-bold">Petition</p>
+                                <p className="text-[24px] font-bold">{petition}</p>
                             </div>
                             <div className="flex">
-                                <p className="text-[#64748B]">Create a petition to crowd <br />
-                                 signatures</p>
+                                <p className="text-[#64748B]">{descrição2} </p>
                             </div>
                         </div>
                     </div>
--- src/routes/CampaingCreate.tsx
+++ src/routes/CampaingCreate.tsx
@@ -10,7 +10,13 @@ function CampaingCreate() {
     <div className="p-2">
       
         
-      <Choose/>
+      <Choose 
+      tittle='1. Choose your campaign style' 
+      subtittle='What its about your campaign?'
+      donation='Donation'
+      petition='Petition'
+      descrição1='Create a donation to receive funds'
+      descrição2='Create a petition to crowd signatures'/>
     </div>
   )
 }
