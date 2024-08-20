6d3d9b89c2743c08bee6141f87e099fd3d93210b
Rafael <Rafael>
2024-08-20 18:37:45 -0300

    ajustes grid

--- src/components/ui/localization.tsx
+++ src/components/ui/localization.tsx
@@ -16,7 +16,7 @@ export function Localization() {
     <>
       <div className="hidden md:flex w-full">
         <div className="flex flex-col gap-y-5">
-          <div className="relative flex  bg-[#F7FEE7]">
+          <div className="relative flex bg-[#F7FEE7]">
             <div className="absolute top-[-5px] left-[10px] bg-lime-200 w-12 h-12 rounded-full flex justify-center items-center border-4 border-lime-100">
               <img src={icon} alt="Dollar Sign" />
             </div>
@@ -24,11 +24,12 @@ export function Localization() {
               <h1>You will get 99.9% of the raised amount</h1>
             </div>
           </div>
-          <div className="flex justify-start py-2 grid grid-cols-2 gap-2 itens-center-start">
+
+          <div className="flex justify-start py-2 grid grid-cols-2 gap-4 items-center">
             <div>
               <h1 className="text-xl font-normal">Localization</h1>
             </div>
-            <div className="w-full ">
+            <div className="w-full">
               <Select>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Rio de Janeiro, Brasil" />
@@ -40,27 +41,29 @@ export function Localization() {
             </div>
           </div>
 
-          <div className=" grid grid-cols-3 gap-2 itens-center-start">
+          <div className="grid grid-cols-2 gap-4 items-start">
             <div className="text-xl">
               <p>Banner Image</p>
             </div>
 
-            <div>
-              <img className="w-[125px] h-[74px] " src={localimage} alt="" />
-            </div>
-
-            <div>
+            <div className="flex gap-3">
+              <img
+                className="w-[125px] h-[74px]"
+                src={localimage}
+                alt="Banner"
+              />
               <Uploadimagens
                 enviar="Click here to upload"
                 descricao="ou drag here your image"
-                tiposEnvio="Recommended 800x600 resolution (max. 1MB) "
+                tiposEnvio="Recommended 800x600 resolution (max. 1MB)"
               />
             </div>
           </div>
         </div>
       </div>
+
       <div className="md:hidden flex flex-col gap-y-4">
-        <div className="relative flex  bg-[#F7FEE7]">
+        <div className="relative flex bg-[#F7FEE7]">
           <div className="absolute top-[-5px] left-[10px] bg-lime-200 w-12 h-12 rounded-full flex justify-center items-center border-4 border-lime-100">
             <img src={icon} alt="Dollar Sign" />
           </div>
@@ -68,26 +71,26 @@ export function Localization() {
             <h1>You will get 99.9% of the raised amount</h1>
           </div>
         </div>
+
         <div className="py-2 mx-5 mt-0 mb-1">
           <h1 className="text-xl font-normal">Localization</h1>
         </div>
-        <div className=" flex flex-col mx-5">
+
+        <div className="flex flex-col mx-5">
           <Input placeholder="Rio de Janeiro, Brasil" />
         </div>
+
         <div className="flex flex-col justify-center items-center">
           <div className="text-xl mt-4">
             <p>Banner</p>
           </div>
-          <div className="flex">
-            <div>
-              <img className=" h-[150px] " src={localimage} alt="" />
-            </div>
-          </div>
-          <div className="mt-4 ">
+
+          <div className="flex flex-col items-center gap-2">
+            <img className="h-[150px]" src={localimage} alt="Banner" />
             <Uploadimagens
               enviar="Click here to upload"
               descricao="ou drag here your image"
-              tiposEnvio="Recommended 800x600 resolution (max. 1MB) "
+              tiposEnvio="Recommended 800x600 resolution (max. 1MB)"
             />
           </div>
         </div>
