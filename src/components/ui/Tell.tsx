4d3f889e2ed9f05d904814dc0c9983d50fce07c2
CamilaMazzochin <mazzochincamila@gmail.com>
2024-08-19 21:31:53 -0300

    Teste props Alexandre

---
+++ src/components/ui/Tell.tsx
@@ -0,0 +1,30 @@
+import { Bold, Italic, Link2, Logs, Menu } from "lucide-react";
+
+export default function Tell() {
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+                <p className="text-[#101828] font-bold text-2xl">2. Tell more about your campaign</p>
+                <p className="text-[#475467] font-normal text-sm">What it's about your campaign?</p>
+            </div>
+            <div className="flex flex-col gap-2">
+                <p className="text-[#1E293B] font-medium">Your campaign title</p>
+                <input className="border border-[#8C9CB1] max-w-md h-8 rounded-sm p-4" type="text" placeholder="Write here your beautiful title 🪴" />
+            </div>
+            <div className="flex flex-col gap-4">
+                <p className="text-[#344054] font-medium">Story</p>
+                <div className="flex flex-col gap-0">
+                    <div className="flex text-[#BABABA] gap-2">
+                        <Bold className="w-5 h-5" />
+                        <Italic className="w-5 h-5" />
+                        <Link2 className="w-5 h-5" />
+                        <Logs className="w-5 h-5" />
+                        <Menu className="w-5 h-5" />
+                    </div>
+                    <textarea className="border border-[#8C9CB1] h-28 max-w-md rounded-sm p-4" itemType="text" placeholder="Tell your story here, give details about your scenario. And explain why you need this help on this campaign" />
+                    <p className="text-[#344054] font-normal text-xs">275 caracteres restantes</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/routes/CampaingCreate.tsx
+++ src/routes/CampaingCreate.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Tell from '../components/ui/Tell'
 
 export const Route = createFileRoute('/CampaingCreate')({
   component: CampaingCreate,
 })
 
 function CampaingCreate() {
-  return <div className="p-2">Coloquem seus componentes aqui</div>
+  return(<Tell/> )
 }
