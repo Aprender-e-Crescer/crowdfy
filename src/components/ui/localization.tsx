31290892847f4019ab8e265921793d820ecf326f
Rafael <Rafael>
2024-08-20 19:19:16 -0300

    localization finalizado

--- src/components/ui/localization.tsx
+++ src/components/ui/localization.tsx
@@ -13,13 +13,13 @@ import { Uploadimagens } from '@/components/ui/UploadImagens'
 export function Localization() {
   return (
     <>
-      <div className="hidden md:flex w-full">
+      <div className="hidden md:flex ">
         <div className="flex flex-col gap-y-5">
           <div className="relative flex rounded-md bg-[#F7FEE7]">
             <div className="absolute top-[-5px] left-[10px] bg-lime-200 w-12 h-12 rounded-full flex justify-center items-center border-4 border-lime-100">
               <img src={icon} alt="Dollar Sign" />
             </div>
-            <div className="py-2 text-center ml-20">
+            <div className=" font-semibold py-2 text-center ml-20">
               <h1>You will get 99.9% of the raised amount</h1>
             </div>
           </div>
@@ -74,7 +74,9 @@ export function Localization() {
         </div>
 
         <div className=" py-2 mx-5 mt-0 mb-1">
-          <h1 className=" text-[#344054] text-xl font-normal">Localization</h1>
+          <h1 className=" text-[#344054] text-[16px]   font-normal">
+            Localization
+          </h1>
         </div>
 
         <div className="flex flex-col mx-5">
@@ -89,7 +91,7 @@ export function Localization() {
         </div>
 
         <div className="flex flex-col justify-center items-center">
-          <div className="text-xl mt-4">
+          <div className="text-[16px] mt-4">
             <p>Banner</p>
           </div>
 
