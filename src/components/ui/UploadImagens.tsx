6456e2aaf340ca542893b27d8f786163fa7f6ef1
GabrielLohnSchefferDEV <lohnscheffergabriel@gmail.com>
2024-08-20 19:14:04 -0300

    Componente finalizado

--- src/components/ui/UploadImagens.tsx
+++ src/components/ui/UploadImagens.tsx
@@ -2,20 +2,25 @@ import simbolo from '..//../assets/CloudArrowUp.png'
 
 export function Uploadimagens({enviar, descricao , tiposEnvio}:{enviar: string, descricao: string, tiposEnvio: string}) {
       return(
-
-      <div className="font-normal">
-                  <div className="ml-28  bg-[#EDEDED] border-4 rounded-full border-[#F6F6F6] h-[56px] w-[56px]">
-                        <img className="my-2.5 ml-2.5  h-[28px] w-[28px] rounded-full" src={simbolo} alt=""/>
+      <div>
+          <div className='flex flex-col items-start '>
+            <div className="flex flex-col items-center" >
+                  <div className=" h-[56px] w-[56px] rounded-full bg-[#EDEDED] border-4  flex items-center justify-center border-[#F6F6F6]">
+                  <img className=" h-[36px] w-[36px] "  src={simbolo} alt=""/>
+                  </div>
+            
+     
+                  <div className='flex gap-1'>
+                  <h1 className="text-lime-500  hover:text-lime-300 font-bold">{enviar}</h1>
+                  <h2 className="text-gray-500">{descricao}</h2>
                   </div>
 
-                  <div className="flex my-1">
-                        <h1 className="text-lime-500  hover:text-lime-300 font-bold">{enviar}</h1>
-                        <h2 className="ml-1 text-gray-500">{descricao}</h2>
-                  </div>
 
-                  <div className='ml-6 text-gray-500 my-1'>
-                       {tiposEnvio}
-                  </div>
-      </div>      
+                 {tiposEnvio}
+            </div>
+            </div>
+            
+      </div>
+      
 )
 }
