19515d642a60c9f7fa4413d6593f254f4697e7e6
enzoguis <enzog0315@gmail.com>
2024-08-20 22:00:01 -0300

    corrigindo erro

--- src/components/personalInfo.tsx
+++ src/components/personalInfo.tsx
@@ -4,9 +4,9 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import { Input } from '@/components/ui/input'
+} from '@/components/select'
+import { RadioGroup, RadioGroupItem } from '@/components/radio-group'
+import { Input } from '@/components/input'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
 
 export default function PersonalInfo() {
