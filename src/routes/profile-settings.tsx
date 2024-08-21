cb9467bf44542b63a1cb4b48b017341762b0d61f
enzoguis <enzog0315@gmail.com>
2024-08-20 22:31:10 -0300

    ajustando profile

--- src/routes/profile-settings.tsx
+++ src/routes/profile-settings.tsx
@@ -4,6 +4,7 @@ import { Localization } from '@/components/localization'
 import { createFileRoute } from '@tanstack/react-router'
 import ImageAvatar from '@/assets/imagemavatar.png'
 import ImageFundo from '@/assets/Image overlay.png'
+import PersonalInfo from '@/components/personalInfo'
 
 export const Route = createFileRoute('/profile-settings')({
   component: ProfileSettings,
@@ -13,27 +14,28 @@ function ProfileSettings() {
   return (
     <>
       <div className=" md:hidden  flex flex-col mx-3 gap-y-4">
-        <div className="flex flex-col gap-x-5">
-          <Localization />
-
-          <div className="flex justify-center ">
-            <Card
-              avatarName="Marcus Dutra"
-              avatarSrc={ImageAvatar}
-              cardDescription="Designer, Rio de Janeiro, Brasil"
-              cardTitle=""
-              donationValue="18.543 XP/20.000 XP"
-              imageSrc={ImageFundo}
-              progressValue={80}
-            />
+        <div className="flex flex-col gap-y-5">
+          <div className="flex flex-col justify-center gap-y-8">
+            <PersonalInfo />
+            <Localization />
           </div>
         </div>
+        <Card
+          avatarName="Marcus Dutra"
+          avatarSrc={ImageAvatar}
+          cardDescription="Designer, Rio de Janeiro, Brasil"
+          cardTitle=""
+          donationValue="18.543 XP/20.000 XP"
+          imageSrc={ImageFundo}
+          progressValue={80}
+        />
+
         <Footer />
       </div>
 
-      <div className=" hidden md:flex flex-col mx-3 gap-y-4">
-        <div className="flex  gap-x-5">
-          <Localization />
+      <div className=" hidden md:flex flex-col mx-3 gap-y-4 justify-center items-center">
+        <div className="flex gap-x-5 justify-center items-center">
+          <PersonalInfo />
           <Card
             avatarName="Marcus Dutra"
             avatarSrc={ImageAvatar}
@@ -43,6 +45,9 @@ function ProfileSettings() {
             imageSrc={ImageFundo}
             progressValue={80}
           />
+        </div>
+        <div className="flex ml-10">
+          <Localization />
         </div>
         <Footer />
       </div>
