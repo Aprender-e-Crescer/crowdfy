563c3ad0bc9e6ebe6ab819f72a982e0f4954cc5a
enzoguis <enzog0315@gmail.com>
2024-08-21 22:07:33 -0300

    ajuste

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import Footer from '@/components/footer'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import Header from '@/componentes/header'
 
 export const Route = createRootRoute({
   component: () => (
