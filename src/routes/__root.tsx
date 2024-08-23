b61b80437281f6c91c0f4e8e759a8342f46482ef
Rafael <Rafael>
2024-08-22 21:03:44 -0300

    add header

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import Header from '@/componentes/header'
 import Footer from '@/components/footer'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
@@ -17,6 +18,7 @@ export const Route = createRootRoute({
         </Link>
       </div>
       <hr />
+      <Header />
       <Outlet />
       <Footer />
       <TanStackRouterDevtools />
