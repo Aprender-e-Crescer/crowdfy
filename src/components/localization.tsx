f8cadcf053c5593171200fd0497eec6482a90b81
enzoguis <enzog0315@gmail.com>
2024-08-25 22:22:11 -0300

    ajustando select mobile

--- src/components/localization.tsx
+++ src/components/localization.tsx
@@ -86,11 +86,16 @@ export function Localization() {
 
         <div className="flex flex-col mx-5">
           <Select>
-            <SelectTrigger className=" text-[#slate/800] w-full">
+            <SelectTrigger className="w-full">
               <SelectValue placeholder="Rio de Janeiro, Brasil" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="matthew">Dois Vizinhos, Brasil</SelectItem>
+              <SelectItem value="doisvizinhos">
+                Dois Vizinhos, Brasil
+              </SelectItem>
+              <SelectItem value="riodejaneiro">
+                Rio de Janeiro, Brasil
+              </SelectItem>
             </SelectContent>
           </Select>
         </div>
