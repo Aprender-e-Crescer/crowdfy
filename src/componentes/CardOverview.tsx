d5db6682335b643b47e65ef2980ff83b72f2f41f
7chaylon <chaylontdasilva@gmail.com>
2024-08-26 21:00:10 -0300

    Finalizado

--- src/componentes/CardOverview.tsx
+++ src/componentes/CardOverview.tsx
@@ -6,48 +6,48 @@ import { LuCheckCheck } from "react-icons/lu";
 
 function CardOverview() {
   return (
-    <div className="border-solid border-[#EAECF0] border-[2px] h-auto w-full lg:w-[1160px] rounded-[12px] p-[24px]">
+    <div className="border-solid border-[#EAECF0] border-[2px] h-auto w-full lg:w-[936px] rounded-[12px] p-[20px]">
       <div className="flex flex-col lg:flex-row justify-between">
-        <h2 className="font-semibold text-[20px] mt-[28px] mb-[28px]">Your campaigns (2)</h2>
-        <div className="flex justify-center lg:ml-[485px] mb-[28px]">
-          <Button variant="BrancoBorda" className="mt-[20px] h-[40px] w-[70px] rounded-br-[0px] rounded-tr-[0px]">
+        <h2 className="font-semibold text-[18px] mt-[28px] mb-[28px]">Your campaigns (2)</h2>
+        <div className="flex justify-center lg:ml-[405px] mb-[28px]">
+          <Button variant="BrancoBorda" className="mt-[20px] h-[40px] w-[50px] rounded-br-[0px] rounded-tr-[0px]">
             All
           </Button>
-          <Button variant="AcinzentadoIcon" className="mt-[20px] h-[40px] w-[154px] rounded-br-[0px] rounded-tr-[0px] rounded-bl-[0px] rounded-tl-[0px]">
+          <Button variant="AcinzentadoIcon" className="mt-[20px] h-[40px] w-[134px] rounded-br-[0px] rounded-tr-[0px] rounded-bl-[0px] rounded-tl-[0px]">
             <div>
               <PiTimer className="h-[20px]" />
             </div>
             <div className="ml-2">In Progress</div>
           </Button>
-          <Button variant="AcinzentadoIcon" className="mt-[20px] h-[40px] w-[154px] rounded-bl-[0px] rounded-tl-[0px]">
+          <Button variant="AcinzentadoIcon" className="mt-[20px] h-[40px] w-[134px] rounded-bl-[0px] rounded-tl-[0px]">
             <LuCheckCheck />
             <div className="ml-2">Concluded</div>
           </Button>
         </div>
       </div>
       <div className="flex flex-col lg:flex-row mb-[20px] lg:ml-[0px]">
-        <div className="mb-[20px] lg:mr-[24px] w-full lg:w-[calc(50%-12px)]">
-          <Card Titulo={<span>Renovate<br />Kingstown<br />Orphanage</span>} Nome={'ANA YAMAMOTO'} NumeroId={'0xbb AC23 43af 98fg'} Imagem={'https://www.brighthope.org/wp-content/uploads/2022/02/tim-trad-sS28i1H39AY-unsplash-800x533.jpg'} CorFundo={"#BEF264"} CorTexto={"#1E293B"} ValorCard={"$2,840.40"} />
+        <div className="mb-[20px] lg:mr-[20px]">
+          <Card Titulo={<span>Celso<br />Kingstown<br />Orphanage</span>} Nome={'ANA YAMAMOTO'} NumeroId={'0xbb AC23 43af 98fg'} Imagem={'https://www.brighthope.org/wp-content/uploads/2022/02/tim-trad-sS28i1H39AY-unsplash-800x533.jpg'} CorFundo={"#BEF264"} CorTexto={"#1E293B"} ValorCard={"$2,840.40"} />
           <Progress value={55} className="mt-2" />
         </div>
-        <div className="mb-[20px] lg:mr-[24px] w-full lg:w-[calc(50%-12px)]">
+        <div className="mb-[20px]">
           <Card Titulo={<span>Install Water<br />Well in<br />Kingstown</span>} Nome={'ANA YAMAMOTO'} NumeroId={'5226 6749 6789 4341'} Imagem={'https://th.bing.com/th/id/OIP.Rsr2pvOkUH8jQy3TajZ6LAAAAA?pid=ImgDet&w=200&h=200&c=7'} CorFundo={"#3F6212"} CorTexto={"#FFFFFF"} ValorCard={"$1,260.20"} />
           <Progress value={30} className="mt-2" />
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row mb-[20px] lg:ml-[0px]">
-        <div className="mb-[20px] lg:mr-[24px] w-full lg:w-[calc(50%-12px)]">
+      <div className="flex flex-col lg:flex-row lg:ml-[0px]">
+        <div className="mb-[20px] lg:mr-[20px]">
           <Card Titulo={<span>Install Water<br />Well in<br />Kingstown</span>} Nome={'ANA YAMAMOTO'} NumeroId={'5226 6749 6789 4341'} Imagem={'https://th.bing.com/th/id/OIP.cKrnleA9Ut-H9geNg99UwgHaFj?pid=ImgDet&w=200&h=150&c=7'} CorFundo={"#BEF264"} CorTexto={"#1E293B"} ValorCard={"$1,260.20"} />
           <Progress value={30} className="mt-2" />
         </div>
-        <div className="mb-[20px] w-full lg:w-[calc(50%-12px)]">
+        <div className="mb-[20px]">
           <Card Titulo={<span>Install Water<br />Well in<br />Kingstown</span>} Nome={'ANA YAMAMOTO'} NumeroId={'5226 6749 6789 4341'} Imagem={'https://th.bing.com/th/id/OIP.cKrnleA9Ut-H9geNg99UwgHaFj?pid=ImgDet&w=200&h=150&c=7'} CorFundo={"#BEF264"} CorTexto={"#1E293B"} ValorCard={"$1,260.20"} />
           <Progress value={30} className="mt-2" />
         </div>
       </div>
       <div className="w-full h-[1px] bg-[#EAECF0] mt-[35px] flex" />
       <div className="flex flex-col lg:flex-row justify-between">
-        <Button variant="letraVerde" className="mt-[12px] lg:mr-[24px]">
+        <Button variant="letraVerde" className="mt-[12px] lg:mr-[20px]">
           Need help?
         </Button>
         <Button variant="BrancoBorda" className="mt-[12px]">
--- src/componentes/Titulo.tsx
+++ src/componentes/Titulo.tsx
@@ -1,11 +1,14 @@
 function Titulo() {
   return (
-    <div className="flex items-center mb-1 justify-end gap-[110px] md:gap-[30px] lg:gap-[145px] mr-6">
-      <h1 className="text-[20px] font-semibold font-sans text-gray-900">Activity</h1>
-      <h2 className="text-[14px] font-medium font-sans text-gray-500">View all</h2>
+    <div className="flex justify-between items-center px-4 lg:px-6 mb-4 lg:mb-1">
+      <h1 className="text-lg lg:text-2xl font-semibold font-sans text-gray-900">
+        Activity
+      </h1>
+      <h2 className="text-sm lg:text-base font-medium font-sans text-gray-500">
+        View all
+      </h2>
     </div>
   );
 }
 
 export default Titulo;
-
