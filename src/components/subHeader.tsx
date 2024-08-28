529ef15a6c3aedf9aac90b60c49dd08a9d754f7a
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-08-28 00:00:21 -0300

    Refatorando SubHeader

--- src/components/subHeader.tsx
+++ src/components/subHeader.tsx
@@ -1,25 +1,26 @@
-import { Button } from "./ui/button"
-import { InputSubHeader } from "./ui/input-sub-header"
+import { Button } from "./ui/button";
+import { InputSubHeader } from "./ui/input-sub-header";
 
-export function SubHeader({buttons} :
-    {
-        buttons: {
-            title: string,
-            variant:  "green" | "ghost",
-            icon: JSX.Element,
-            route: string,
-        }[]
-    }
-) {
-    return (
-        <div className="flex justify-between">
-            <div className="flex gap-3 mr-5">
-                {buttons.map((button) => (
-                    <Button key={button.title} variant={button.variant}>
-                      {button.icon} {button.title}
-                    </Button>))}
-            </div>
-            <InputSubHeader />
-        </div>
-    )
+interface Props {
+  buttons: {
+    title: string;
+    variant: "green" | "ghost";
+    icon: JSX.Element;
+    route: string;
+  }[];
+}
+
+export function SubHeader({ buttons }: Props) {
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-3 mr-5">
+        {buttons.map(({ title, variant, icon }) => (
+          <Button key={title} variant={variant}>
+            {icon} {title}
+          </Button>
+        ))}
+      </div>
+      <InputSubHeader />
+    </div>
+  );
 }
