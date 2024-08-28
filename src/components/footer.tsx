66acf4b059f369a842abd9129aec479c3eb95a42
Samuel Molinari <samuelmolinari8@gmail.com>
2024-08-27 21:10:23 -0300

    tirando bold dos links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,19 +17,19 @@ export default function Footer() {
         <div className="flex justify-center gap-x-16 py-20 flex-1 px-8">
           <div className="flex flex-col flex-1 items-start max-w-40">
             <h1 className="text-lime-700 font-semibold">Home</h1>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">My favorites</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Recents</p>
             </Link>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
             <h1 className="font-semibold">Explore</h1>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Lists</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="flex text-slate-600">
                 Maps <Badge variant="success">New</Badge>
               </p>
@@ -39,28 +39,28 @@ export default function Footer() {
             <h1 className="font-semibold">Campaigns</h1>
 
             <p className="text-slate-600 ">Home</p>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="flex text-slate-600">
                 Campaigns <Badge variant="success">12</Badge>
               </p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               {' '}
               <p className="flex text-slate-600">
                 Donations <Badge variant="success">4</Badge>
               </p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               {' '}
               <p className="text-slate-600">Analytics</p>
             </Link>
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
             <h1 className="font-semibold">Profile</h1>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Settings</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="flex text-slate-600">
                 Notifications <Badge variant="success">10</Badge>
               </p>
@@ -68,19 +68,19 @@ export default function Footer() {
           </div>
           <div className="flex flex-col flex-1 items-start max-w-40">
             <h1 className="font-semibold">Resources</h1>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">How to use crouwdfy</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Docs</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Legal Terms</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Blog</p>
             </Link>
-            <Link to="/" className="[&.active]:font-bold">
+            <Link to="/" className="[&.active]">
               <p className="text-slate-600">Merch</p>
             </Link>
           </div>
@@ -106,12 +106,12 @@ export default function Footer() {
               Home
             </AccordionTrigger>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 My favorites
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Recents
               </Link>
             </AccordionContent>
@@ -121,12 +121,12 @@ export default function Footer() {
               Explore
             </AccordionTrigger>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Lists
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Maps <Badge variant="success">New</Badge>
               </Link>
             </AccordionContent>
@@ -136,24 +136,24 @@ export default function Footer() {
               Campaigns
             </AccordionTrigger>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 {' '}
                 Home
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 {' '}
                 Campaigns<Badge variant="success">12</Badge>
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Donations<Badge variant="success">4</Badge>
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Analytics
               </Link>
             </AccordionContent>
@@ -163,12 +163,12 @@ export default function Footer() {
               Profile
             </AccordionTrigger>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Settings
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Notifications<Badge variant="success">10</Badge>
               </Link>
             </AccordionContent>
@@ -178,27 +178,27 @@ export default function Footer() {
               Resources
             </AccordionTrigger>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 How to use crouwdfy
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Docs
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Legal Terms
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 Blog
               </Link>
             </AccordionContent>
             <AccordionContent className="bg-muted text-muted-foreground p-4">
-              <Link to="/" className="[&.active]:font-bold">
+              <Link to="/" className="[&.active]">
                 {' '}
                 Merch
               </Link>
