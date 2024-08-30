b77ffbaa16b70c8342a62d84acf5d469ddc3e631
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-08-30 00:41:23 -0300

    Resolvendo erros de importação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { Roadmap } from '@/components/ui/roadmap-comming-soon'
-import { createFileRoute } from '@tanstack/react-router'
+import { Roadmap } from "@/components/roadmap-comming-soon";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
     <div className="p-2">
       <Roadmap />
     </div>
-  )
+  );
 }
