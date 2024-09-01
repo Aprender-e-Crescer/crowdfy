d0a7251c906e7512bf805f81b06278d0c8a4e197
Yuosssef <yuossefpolidorio@gmail.com>
2024-08-31 23:17:50 -0300

    ajuste da responsividade

--- src/routes/Campaigns.tsx
+++ src/routes/Campaigns.tsx
@@ -15,7 +15,7 @@ function Campaigns() {
       <div className='mt-[24px] mb-[16px] md:mt-[48px] md:mb-[32px]'>
         <TituloPage />
       </div>
-      <div className='flex flex-col md:flex-row'>
+      <div className='flex flex-col md:flex-row h-full w-full mr9'>
         <div>
           <div className='mr-4'>
             <CardOverview />
@@ -28,7 +28,7 @@ function Campaigns() {
         <div className='flex flex-col md:ml-5 mt-6 md:mt-0'>
           <Titulo />
           {/* Envolva as atividades em uma div com rolagem vertical em telas pequenas */}
-          <div className='flex flex-col gap-4 overflow-y-auto max-h-[400px] md:max-h-full'>
+          <div className='flex flex-col gap-4 overflow-y-auto max-h-[400px] md:max-h-full ml-7 MR-7'>
             <ViewActivity />
             <ViewActivity />
             <ViewActivity />
