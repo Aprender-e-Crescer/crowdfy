c53887ad230cdd4491cb0cec78fa38b5992f39b5
LuisSchimitt <luis.schimitt110405@gmail.com>
2024-09-02 19:01:14 -0300

    feita a responsividade

--- src/components/cardsViews.tsx
+++ src/components/cardsViews.tsx
@@ -2,7 +2,7 @@ import FooterOverview from "@/components/footerOverview";
 
 function CardsViews() {
   return (
-    <div className="flex flex-wrap justify-center space-y-8 sm:space-y-0 sm:space-x-5">
+    <div className="md:flex-col lg:flex-row flex flex-col justify-center sm:gap-6 gap-3">
       <FooterOverview titulo="Donations" valor="$1,280" link="View analytics" />
       <FooterOverview titulo="Signatures" valor="6,785" link="View report" />
       <FooterOverview titulo="Likes" valor="77" link="View report" />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,8 @@ import settingsImage from "@/assets/perfil.png";
 import iconButom from "@/assets/iconButom.png";
 import { Link } from "@tanstack/react-router";
 
+
+
 function Header() {
   const [activeLink, setActiveLink] = useState("Campaigns");
   const [isTransparent, setIsTransparent] = useState(false);
@@ -26,25 +28,19 @@ function Header() {
 
   return (
     <header
-
-      className={`2xl:ml-[218px] ml-[16px]  flex flex-wrap items-center justify-between ${
-        isTransparent ? "bg-transparent" : "bg-white"
-      } shadow-sm px-4 py-3 md:px-8 transition-all duration-300`}
-
       className={`flex flex-wrap items-center justify-between ${isTransparent ? "bg-transparent" : "bg-white"
-        } px-2 pb-1 pt-3 md:px-2 transition-all duration-300`}
-
+        } shadow-sm py-3  transition-all duration-300`}
     >
       {/* Logo */}
-
-      <div className="flex items-center flex-grow-0  ">
-        <div className="flex items-center gap-1">
+      <div className="flex items-center flex-grow-0 ml-3 md:ml-[20px] lg:ml-[90px]">
+        <div className="flex items-center space-x-1">
           <img src={logoImage} alt="Logo" className="w-8 h-8" />
           <div className="text-black text-xl font-bold">Crowdfy</div>
         </div>
       </div>
 
-      <nav className="hidden lg:flex ml-8 gap-4 flex-grow">
+      {/* Links de navegação (escondidos em telas pequenas) */}
+      <nav className="hidden md:flex ml-8 flex-grow gap-5">
         <Link
           to=""
           className={linkClasses("Home")}
@@ -53,13 +49,12 @@ function Header() {
           Home
         </Link>
         <Link
-          to="/explore"
+          to=""
           className={linkClasses("Explore")}
           onClick={() => handleLinkClick("Explore")}
         >
           Explore
         </Link>
-        
         <Link
           to="/Campaigns"
           className={linkClasses("Campaigns")}
@@ -76,47 +71,48 @@ function Header() {
         </Link>
       </nav>
 
-      <div className="flex-grow">
-        <div className="flex items-center gap-4">
-          <button className="p-2 w-10 h-10 rounded-lg flex items-center justify-center relative z-10000">
-            <div className="bg-[#EDEDED] border-[#F6F6F6] absolute inset-0 border-2 rounded-lg flex items-center justify-center">
-              <div className="w-6 h-6 border-2 border-gray-400 rounded-full flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-4 h-4 text-gray-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-              </div>
+      {/* Menu Hamburguer Responsivo */}
+
+      {/* Ações do usuário */}
+      <div className="flex items-center space-x-2 mr-3 lg:mr-[90px] md:ml-[20px]">
+        <button className="p-2 w-10 h-10 rounded-lg flex items-center justify-center relative z-10000">
+          <div className="bg-[#EDEDED] border-[#F6F6F6] absolute inset-0 border-2 rounded-lg flex items-center justify-center">
+            <div className="w-6 h-6 border-2 border-gray-400 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-4 h-4 text-gray-600"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>
             </div>
-          </button>
-
-          <div className="bg-[#EDEDED] border-[#F6F6F6] border-2 p-2 w-10 h-10 rounded-full shadow-md flex items-center justify-center">
-            <img
-              src={settingsImage}
-              alt="Avatar do Usuário"
-              className="w-8 h-8 rounded-full object-cover"
-            />
           </div>
+        </button>
 
-          <div className="p-2 rounded-lg w-10 h-10 bg-[#EDEDED] border-[#F6F6F6] border-2 shadow-md flex items-center justify-center">
-            <img
-              src={iconButom}
-              alt="Ícone de Configurações"
-              className="w-full h-full bg-[#EDEDED]"
-            />
-          </div>
-        
+        <div className="bg-[#EDEDED] border-[#F6F6F6] border-2 p-2 w-10 h-10 rounded-full shadow-md flex items-center justify-center">
+          <img
+            src={settingsImage}
+            alt="Avatar do Usuário"
+            className="w-8 h-8 rounded-full object-cover"
+          />
+        </div>
 
-        <div className="flex lg:hidden">
+        <div className="p-2 rounded-lg w-10 h-10 bg-[#EDEDED] border-[#F6F6F6] border-2 shadow-md flex items-center justify-center">
+          <img
+            src={iconButom}
+            alt="Ícone de Configurações"
+            className="w-full h-full bg-[#EDEDED]"
+          />
+        </div>
+
+        <div className="md:hidden">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             className="p-2 rounded-lg w-10 h-10 bg-[#EDEDED] border-[#F6F6F6] border-2 shadow-md flex items-center justify-center"
@@ -136,9 +132,8 @@ function Header() {
               />
             </svg>
           </button>
-        </div>
+
           {/* Dropdown Menu (exibido quando o botão é clicado) */}
-          
           {isMenuOpen && (
             <div className="absolute top-14 left-0 w-full bg-white shadow-lg z-50">
               <Link
