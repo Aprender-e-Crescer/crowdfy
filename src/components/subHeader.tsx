7c16fbe222e1a8558f4e61022addd727e9025dae
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-09-02 22:28:54 -0300

    Mais ajuste nos subheader

--- src/components/subHeader.tsx
+++ src/components/subHeader.tsx
@@ -5,11 +5,8 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoreVertical } from "lucide-react";
-import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
 interface Props {
   buttons: {
     title: string;
@@ -23,7 +20,7 @@ export function SubHeader({ buttons }: Props) {
   return (
     <>
       <hr />
-      <div className="flex justify-between lg:pt-6 lg:pb-6 pt-3 pb-3 max-[450px]:gap-2 ml-2">
+      <div className="flex justify-between lg:pt-6 lg:pb-6 pt-3 pb-3 max-[450px]:gap-2 ml-2 lg:mx-24">
         <div className="gap-3 mr-2 sm:mr-5 hidden lg:flex">
           {buttons.map(({ title, variant, icon, route }) => (
             <Link to={route} key={title}>
@@ -36,16 +33,12 @@ export function SubHeader({ buttons }: Props) {
         <div className="lg:hidden">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="lightYellow">
-                Actions
-              </Button>
+              <Button variant="lightYellow">Actions</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               {buttons.map(({ title, route }) => (
                 <DropdownMenuItem key={title}>
-                  <Link to={route}>
-                    {title}
-                  </Link>
+                  <Link to={route}>{title}</Link>
                 </DropdownMenuItem>
               ))}
             </DropdownMenuContent>
@@ -57,13 +50,3 @@ export function SubHeader({ buttons }: Props) {
     </>
   );
 }
-
-
-
-<DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
