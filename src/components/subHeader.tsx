5c29a5379a248190d506ebaad08991a34bf2e630
JonatasFiorentin <jonatasfiorentin@gmail.com>
2024-09-02 22:36:31 -0300

    Ajustando subHeader

--- src/components/subHeader.tsx
+++ src/components/subHeader.tsx
@@ -1,29 +1,29 @@
-import { Link } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
-import { InputSubHeader } from "@/components/input-sub-header";
+import { Link } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
+import { InputSubHeader } from '@/components/input-sub-header'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { MoreVertical } from "lucide-react";
-import { DropdownMenuSeparator } from "@radix-ui/react-dropdown-menu";
+} from '@/components/ui/dropdown-menu'
+import { MoreVertical } from 'lucide-react'
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
 interface Props {
   buttons: {
-    title: string;
-    variant: "lightYellow" | "ghost";
-    icon: JSX.Element;
-    route: string;
-  }[];
+    title: string
+    variant: 'lightYellow' | 'ghost'
+    icon: JSX.Element
+    route: string
+  }[]
 }
 
 export function SubHeader({ buttons }: Props) {
   return (
     <>
       <hr />
-      <div className="flex justify-between lg:pt-6 lg:pb-6 pt-3 pb-3 max-[450px]:gap-2 ml-2">
+      <div className="flex justify-between max-[450px]:flex-col lg:pt-6 lg:pb-6 pt-3 pb-3 max-[450px]:gap-2 ml-2">
         <div className="gap-3 mr-2 sm:mr-5 hidden lg:flex">
           {buttons.map(({ title, variant, icon, route }) => (
             <Link to={route} key={title}>
@@ -36,16 +36,12 @@ export function SubHeader({ buttons }: Props) {
         <div className="lg:hidden">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="lightYellow">
-                Actions
-              </Button>
+              <Button variant="lightYellow">Actions</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               {buttons.map(({ title, route }) => (
                 <DropdownMenuItem key={title}>
-                  <Link to={route}>
-                    {title}
-                  </Link>
+                  <Link to={route}>{title}</Link>
                 </DropdownMenuItem>
               ))}
             </DropdownMenuContent>
@@ -55,15 +51,13 @@ export function SubHeader({ buttons }: Props) {
       </div>
       <hr />
     </>
-  );
+  )
 }
 
-
-
-<DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
+;<DropdownMenuContent>
+  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+  <DropdownMenuItem>Profile</DropdownMenuItem>
+  <DropdownMenuItem>Billing</DropdownMenuItem>
+  <DropdownMenuItem>Team</DropdownMenuItem>
+  <DropdownMenuItem>Subscription</DropdownMenuItem>
+</DropdownMenuContent>
