36b977c501bb3c1eef749ffb05355499d9498cc2
JonatasFiorentin <jonatasfiorentin@gmail.com>
2024-09-03 18:25:12 -0300

    Tirando Import SubHeader, Ajustando Botoes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
-import { SubHeader } from '@/components/subHeader'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
