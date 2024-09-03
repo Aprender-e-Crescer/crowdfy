5436bfe3ecab960de5940d107cfad5708834a413
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-09-02 21:26:05 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,20 +8,22 @@ export const Route = createFileRoute("/")({
 
 export function HomePage() {
   return (
-    <div className="bg-[url('@/assets/fundoHome.png')] bg-cover bg-center relative h-[100vh]">
-      <div className="absolute inset-0 flex items-center justify-center h-[923px]">
-        <div className="bg-white max-w-[658px] rounded-lg">
-          <Card
-            title="Help us Teach English for Kids on China Countryside"
-            descrition="I need about 9.000 signatures to deliver to my mayor in two weeks and get that grant! Check more about our project and if you want to see how we are changing lovely children lifes on China countryside just teaching how they can use the internet."
-            userName="Mateus Rodrigues"
-            location="São Paulo - Brazil"
-            barNumbers="4.533/9.000"
-            backgroundImage={false}
-          />
-          <RoadmapFooter />
+    <>
+      <div className="bg-[url('@/assets/fundoHome.png')] bg-cover bg-no-repeat bg-center">
+        <div className="flex justify-center items-center">
+          <div className="mb-10 max-w-[850px]">
+            <Card
+              title="Help us Teach English for Kids on China Countryside"
+              descrition="I need about 9.000 signatures to deliver to my mayor in two weeks and get that grant! Check more about our project and if you want to see how we are changing lovely children lifes on China countryside just teaching how they can use the internet."
+              userName="Mateus Rodrigues"
+              location="São Paulo - Brazil"
+              barNumbers="4.533/9.000"
+              backgroundImage={false}
+            />
+            <RoadmapFooter />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
